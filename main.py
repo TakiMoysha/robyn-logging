0c1d3e062c62e230b36db3d4c74816f076d96d89
TakiMoysha <mpwema782@gmail.com>
2023-11-30 22:11:14 +0100

    updated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pathlib
 
-from robyn import Robyn, jsonify, Request
+from robyn import Robyn, jsonify, Request, serve_html
 from robyn.templating import JinjaTemplate
 
 app = Robyn(__file__)
@@ -9,11 +9,15 @@ TEMPLATES = JinjaTemplate(current_file_path / "templates")
 
 @app.get("/")
 async def h(request):
+    return serve_html("./templates/index.html")
+
+@app.get("/json")
+async def json(request):
     return jsonify({"msg": "hello world!", "version": "0.0.2"})
 
 @app.get("/logger")
 async def logger(request: Request):
     context = { "id": request.ip_addr }
-    return TEMPLATES.render_template(template_name="index.html", **context)
+    return TEMPLATES.render_template(template_name="logging.html", **context)
 
 app.start(port=8080)
